fa55859051a64ac22d7e3052187d4d0008198423
samar abdelhmeed <samar.abdelhmeed77@gmail.com>
2022-04-17 10:22:58 +0200

    working in frontend

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+
+
+# An ERC20 token faucet on the reposten Testnet 
+
+This Dapp allows users to receive 100 faucet on the reposten test net and to send faucet to other addresses. 
+
+
 # Basic Sample Hardhat Project
 
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import TokenSend from './components/TokenSend.js'
+import Faucet from './components/Faucet.js'
+import FCTToken from './artifacts/contracts/FCTToken.sol/FCTToken.json'
+
+import { Container, Row, Col, Card } from 'react-bootstrap'
+
 
 function App() {
+
+  const Token = FCTToken;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    only compatible with the reposten testnet
+    <Card.Body>
+    <Container>
+    <Row className="justify-content-md-center">
+      <Col>
+      <Faucet  tokenContract={Token}/>
+      </Col>
+      <Col>
+      <TokenSend tokenContract={Token}/>
+      </Col>
+    </Row>
+    </Container>
+    </Card.Body>
+    </div>
+
     </div>
   );
 }
